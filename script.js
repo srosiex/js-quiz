aed191dde87814c7253aab325a3fd60d306f7498
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-08-08 16:42:37 -0500

    added function to enable questions to show after start btn is clicked on

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-function startGame(){
+window.onload=function(){
 
-}
+    const startButton = document.getElementById('start-btn')
+    const questionContainerElement = document.getElementById('question-container')
+    let shuffledQuestions, currentQuestionIndex
+    const questionElement = document.getElementById('question')
+    const answerButtonsElement = document.getElementById('answer-buttons')
 
-function setNextQuestion(){
+    startButton.addEventListener('click', startGame)
 
-}
+    function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+    }
 
-function selectAnswer(){
-    
+    function setNextQuestion(){
+        showQuestion(shuffledQuestions[currentQuestionIndex])
+    }
+
+    function showQuestion(question){
+        questionElement.innerText = question.question
+    }
+
+    function selectAnswer(){
+
+    }
+
+    const questions = [
+        {
+            question: 'What is 2 + 2',
+            answers: [
+                {text: '4', correct: true},
+                {text: '22', correct: false}
+            ]
+        }
+    ]
 }
