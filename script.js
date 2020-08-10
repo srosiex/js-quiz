f4084ced213ab28c449df198c4a2f9b7019acc25
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-08-10 08:00:59 -0500

    updated hover on button. adding even listeners to questions and next btn

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload=function(){
-
     const startButton = document.getElementById('start-btn')
+    const nextButton = document.getElementById('next-btn')
     const questionContainerElement = document.getElementById('question-container')
     let shuffledQuestions, currentQuestionIndex
     const questionElement = document.getElementById('question')
@@ -17,15 +17,53 @@ window.onload=function(){
     }
 
     function setNextQuestion(){
+        resetState()
         showQuestion(shuffledQuestions[currentQuestionIndex])
     }
 
     function showQuestion(question){
         questionElement.innerText = question.question
+        question.answers.forEach(answer => {
+            const button = document.createElement('button')
+            button.innerText = answer.text
+            button.classList.add('btn')
+            if(answer.correct){
+                button.dataset.correct = answer.correct
+            }
+            button.addEventListener('click', selectAnswer)
+            answerButtonsElement.appendChild(button)
+        })
     }
 
-    function selectAnswer(){
+    function resetState(){
+        nextButton.classList.add('hide')
+        while (answerButtonsElement.firstChild){
+            answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+        }
+    }
 
+    function selectAnswer(e){
+        const selectedButton = e.target
+        const correct = selectedButton.dataset.correct
+        setStatusClass(document.body, correct)
+        Array.from(answerButtonsElement.children).forEach(button => {
+            setStatusClass(button, button.dataset.correct)
+        })
+    }
+
+    function setStatusClass(element, correct){
+        clearStatusClass(element)
+        if(correct){
+            element.classList.add('correct')
+        }else{
+            element.classList.add('wrong')
+        }
+        nextButton.classList.remove('hide')
+    }
+
+    function clearStatusClass(element){
+        element.classList.remove('correct')
+        element.classList.remove('wrong')
     }
 
     const questions = [
